32f309ab64e242bf7750406e430f8b6b6d23c950
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:46:54 +0300

    Add project files.

---
+++ Pangram/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Pangram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Введіть речення:");
+            string input = Console.ReadLine().ToLower(); 
+
+      
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+           
+            bool isPangram = alphabet.All(c => input.Contains(c));
+
+           
+            if (isPangram)
+            {
+                Console.WriteLine("Речення є панграмою.");
+            }
+            else
+            {
+                Console.WriteLine("Речення не є панграмою.");
+            }
+        }
+    }
+}
